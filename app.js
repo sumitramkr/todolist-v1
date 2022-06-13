14d58c33a97c37ace52e68f173bcb4250eaac2f1
sumitramkr <sumitramkr77e@gmail.com>
2022-06-13 12:20:48 +0530

    added ejs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
     res.send("Hi");
 });
