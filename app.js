f761dca24e0bf61079ed88b4e306a7e2daf3ef5e
sumitramkr <sumitramkr77e@gmail.com>
2022-06-13 12:18:16 +0530

    2nd commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.send("Hi");
+});
+
+app.listen(4700, function(req, res) {
+    console.log("server is live on port 4700");
+});
